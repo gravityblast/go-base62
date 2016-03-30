b071168592461a31a4ba3760db935e0bff6c8485
Cui Yingjie <cuiyingjie@gmail.com>
2016-03-31 07:12:58 +0800

    change input and output parameter from int to uint

--- base62.go
+++ base62.go
@@ -2,52 +2,53 @@
 package base62
 
 import (
-  "math"
-  "bytes"
+	"bytes"
+	"math"
 )
 
 // characters used for conversion
 const alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 // converts number to base62
-func Encode(number int) string {
-  if number == 0 {
-    return string(alphabet[0])
-  }
+func Encode(number uint) string {
+	if number == 0 {
+		return string(alphabet[0])
+	}
 
-  chars := make([]byte, 0)
+	chars := make([]byte, 0)
 
-  length := len(alphabet)
+	length := uint(len(alphabet))
 
-  for number > 0 {
-    result    := number / length
-    remainder := number % length
-    chars   = append(chars, alphabet[remainder])
-    number  = result
-  }
+	for number > 0 {
+		result := number / length
+		remainder := number % length
+		chars = append(chars, alphabet[remainder])
+		number = result
+	}
 
-  for i, j := 0, len(chars) - 1; i < j; i, j = i + 1, j - 1 {
-    chars[i], chars[j] = chars[j], chars[i]
-  }
+	for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
+		chars[i], chars[j] = chars[j], chars[i]
+	}
 
-  return string(chars)
+	return string(chars)
 }
 
 // converts base62 token to int
-func Decode(token string) int {
-  number := 0
-  idx    := 0.0
-  chars  := []byte(alphabet)
+func Decode(token string) uint {
+	var number uint
+	number = 0
+	idx := 0.0
+	chars := []byte(alphabet)
 
-  charsLength := float64(len(chars))
-  tokenLength := float64(len(token))
+	charsLength := float64(len(chars))
+	tokenLength := float64(len(token))
 
-  for _, c := range []byte(token) {
-    power := tokenLength - (idx + 1)
-    index := bytes.IndexByte(chars, c)
-    number += index * int(math.Pow(charsLength, power))
-    idx++
-  }
+	for _, c := range []byte(token) {
+		power := tokenLength - (idx + 1)
+		index := uint(bytes.IndexByte(chars, c))
+		number += index * uint(math.Pow(charsLength, power))
+		idx++
+	}
 
-  return number
+	return number
 }
